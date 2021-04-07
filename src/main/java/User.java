528151a007c10ce57dd3682c03c69d3d18bae131
roshansabale@yahoo.com <roshansabale@yahoo.com>
2021-04-07 16:18:45 +0530

    Refactored: Ability to write data from CSV to JSON using GSON library

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -2,7 +2,7 @@ import com.opencsv.bean.CsvBindByName;
 
 public class User {
 
-    @CsvBindByName(column = "name")
+    @CsvBindByName()
     private String name;
     @CsvBindByName
     private String email;
@@ -11,6 +11,45 @@ public class User {
     @CsvBindByName
     private String country;
 
+    /*public User(String name, String email, String phone, String country) {
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.country = country;
+    }*/
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
     @Override
     public String toString() {
         return "User{" +
@@ -18,6 +57,6 @@ public class User {
                 ", email='" + email + '\'' +
                 ", phone='" + phone + '\'' +
                 ", country='" + country + '\'' +
-                '}';
+                +'}';
     }
 }
--- src/main/java/UserCSVReader.java
+++ src/main/java/UserCSVReader.java
@@ -1,25 +1,45 @@
+import com.google.gson.Gson;
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
+import com.opencsv.bean.StatefulBeanToCsv;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.Reader;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 
 public class UserCSVReader {
-    static final String FILE_NAME = "D:\\java work\\BridgeLabzProjectsAssignments\\OpenCSV\\Resource\\User.csv";
+    static final String FILE_NAME = "D:\\java work\\BridgeLabzProjectsAssignments\\OpenCSV\\Resource\\User1.csv";
+    static final String GSON_FILE_NAME = "D:\\java work\\BridgeLabzProjectsAssignments\\OpenCSV\\Resource\\User.json";
+
     public static void main(String[] args) throws IOException {
-        Reader bufferedReader = Files.newBufferedReader(Paths.get(FILE_NAME));
-        CsvToBean<User> csvToBean = new CsvToBeanBuilder(bufferedReader).withType(User.class).withIgnoreLeadingWhiteSpace(true).build();
-        List<User> iterator = csvToBean.parse();
-        System.out.println(iterator);
-       /* while(iterator.hasNext()) {
-            User user = iterator.next();
-            System.out.println(user);
-        }*/
+        try {
+            Reader bufferedReader = Files.newBufferedReader(Paths.get(FILE_NAME));
+            CsvToBean<User> csvToBean = new CsvToBeanBuilder(bufferedReader)
+                    .withType(User.class)
+                    .withIgnoreLeadingWhiteSpace(true)
+                    .build();
+            List<User> users = csvToBean.parse();
+            Gson gson = new Gson();
+            String json = gson.toJson(users);
+            FileWriter writer = new FileWriter(GSON_FILE_NAME);
+            writer.write(json);
+            writer.close();
+            BufferedReader bufferedReader1 = new BufferedReader(new FileReader(GSON_FILE_NAME));
+            User[] objects = gson.fromJson(bufferedReader1,User[].class);
+            List<User> userlist = Arrays.asList(objects);
+            for (User user : users) {
+                System.out.println("Name -> " + user.getName());
+                System.out.println("Email -> " + user.getEmail());
+                System.out.println("Phone -> " + user.getPhone());
+                System.out.println("Country -> " + user.getCountry());
+                System.out.println("=========================");
+            }
+        }catch (IOException exception) {
+            exception.getMessage();
+        }
     }
 }
